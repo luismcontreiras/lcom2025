0d2c11d350a2a24bf9052f566dfc26f58880fe6e
framoz <100444142+framoz@users.noreply.github.com>
2025-03-01 13:41:42 +0000

    Added comment on tested functions

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -29,14 +29,14 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
+//tested
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 	uint8_t *st = malloc(sizeof(uint8_t));
   if(timer_get_conf(timer, st) != 0) return 1;
 	if(timer_display_conf(timer, *st, field) != 0) return 1;
     return 0;
 }
-
+//tested
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
   
   if(timer_set_frequency(timer, freq) != 0) return 1;
