b0bdba2d44c5eeb72f564f7383a995e601cdf3ba
Luís Ferreira <up202007664@up.pt>
2025-05-29 02:16:01 +0100

    Implement double buffering in game engine for improved rendering performance

--- proj/src/engine/graphics.h
+++ proj/src/engine/graphics.h
@@ -9,6 +9,7 @@
 
 
 vbe_mode_info_t mode_info;
+extern uint8_t *video_mem;  // Make video memory accessible
 
 int(set_to_video_mode)(uint16_t submode);
 int(set_to_text_mode)();
@@ -19,6 +20,12 @@ int(set_color)(uint32_t color, uint32_t *new_color);
 int (vg_draw_pixel)(uint16_t x, uint16_t y, uint32_t color);
 int (vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
 int (vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
+
+// Double buffering functions
+int (vg_draw_pixel_buffer)(uint8_t *buffer, uint16_t x, uint16_t y, uint32_t color, uint16_t screen_width, uint8_t bytes_per_pixel);
+int (vg_draw_rectangle_buffer)(uint8_t *buffer, uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color, uint16_t screen_width, uint8_t bytes_per_pixel);
+int (vg_clear_buffer)(uint8_t *buffer, uint32_t size, uint32_t color, uint8_t bytes_per_pixel);
+int (vg_copy_buffer)(uint8_t *dest, uint8_t *src, uint32_t size);
 
 int (draw_xpm)(uint16_t xi, uint16_t yi,xpm_map_t xpm);
 
